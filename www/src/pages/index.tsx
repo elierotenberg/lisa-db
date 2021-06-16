a25498ca42db94f49c8d5ee4ce6d189ff74ea5fd
Elie Rotenberg <elie@rotenberg.io>
2021-06-16 11:53:11 +0200

    Create www file hierarchy

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -1,25 +1,7 @@
-import { ServerInfo } from "@directus/sdk";
-import { GetStaticProps } from "next";
 import { FunctionComponent } from "react";
 
-import { getDirectusFromEnv } from "../lib/Directus";
+import { lorem } from "../lib/Lorem";
 
-type IndexPageProps = {
-  readonly info: ServerInfo;
-};
-
-const IndexPage: FunctionComponent<IndexPageProps> = ({ info }) => (
-  <div>{JSON.stringify(info)}</div>
-);
-
-export const getStaticProps: GetStaticProps<IndexPageProps> = async () => {
-  const directus = await getDirectusFromEnv();
-  const info = await directus.server.info();
-  return {
-    props: {
-      info,
-    },
-  };
-};
+const IndexPage: FunctionComponent = () => <div>{lorem(100)}</div>;
 
 export default IndexPage;
